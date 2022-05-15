a30f19ea7f10b465e285662b0f6b4ae9d515173f
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-05-14 19:58:27 -0500

    web dependency and simple hello message

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "Hello";
+	}
 }
